a9843ff92ce636dde9bbc2a70bbbdf756a2ee616
node1 <nodepr01@gmail.com>
2022-11-26 20:26:44 +0530

    Added line on app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey Amit, app has been deployed'));
+app.get('/', (req, res) => res.send('<h1>Hey Amit!, <br>Node App has been deployed with the help of AWS pipeline</h1>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
